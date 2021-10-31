ef1b67c993a58cf11466338fd85c0fcecae36fc8
Andrei Merzliakov <codymaverick@bk.ru>
2021-10-31 20:29:08 +0300

    C HAPTER 6: A PPLYING M ONGO DB TO O UR P ROJECT

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const { application } = require('express')
 const express = require('express')
 const path = require('path')
 const app = new express()
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const BlogPost = require('./models/BlogPost.js')
 
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser:true});
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
@@ -11,10 +16,13 @@ app.listen(3000,()=>{
     console.log("App listening on port 3000")
 })
 
-app.get('/',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname,'pages/index.html'))
-    res.render('index')
+app.get('/',async(req,res)=>{
+    const blogposts = await BlogPost.find({})
+    res.render('index',{
+        blogposts
+    });
 })
+
 app.get('/about',(req,res)=>{
     //res.sendFile(path.resolve(__dirname,'pages/about.html'))
     res.render('about')
@@ -23,7 +31,18 @@ app.get('/contact',(req,res)=>{
     //res.sendFile(path.resolve(__dirname,'pages/contact.html'))
     res.render('contact')
 })
-app.get('/post',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname,'pages/post.html'))
-    res.render('post')
+app.get('/post/:id',async(req,res)=>{
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post',{
+        blogpost
+    })
+})
+app.get('/posts/new',(req,res)=>{
+    res.render('create')
+})
+
+app.post('/posts/store',async(req,res)=>{
+    // model create a new doc with browser date
+    await BlogPost.create(req.body)
+    res.redirect('/')
 })
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -3,7 +3,12 @@ const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted:{ /* can declare property type with an abject like this because we need 'default' */
+        type: Date,
+        default: new Date()
+    }
 });
 
 const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
