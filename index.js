da7d0a810473fc4b3821bee4ddf349da3b775518
Andrei Merzliakov <codymaverick@bk.ru>
2021-11-03 22:49:43 +0300

    C HAPTER 8: I NTRODUCTION TO E XPRESS M IDDLEWARE

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const mongoose = require('mongoose')
 const BlogPost = require('./models/BlogPost.js')
 const fileUpload = require('express-fileupload')
 
+const validationMiddleWare = (req,res,next) => {
+    if(req.files == null
+        ||req.body.body == ''
+        ||req.body.title == ''){
+        return res.redirect('/posts/new')
+    }
+    next()
+}
+
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(fileUpload())
+app.use('/posts/store', validationMiddleWare)
 
 app.listen(3000, () => {
     console.log("App listening on port 3000")
